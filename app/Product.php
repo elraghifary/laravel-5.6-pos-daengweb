62e1a72f7017025c345a99093e03236118a0e7b8
Elra Ghifary <elraghifary@gmail.com>
2018-07-21 21:28:39 +0700

    modify product model

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = ['code', 'name', 'description', 'stock', 'price', 'category_id', 'photo'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
