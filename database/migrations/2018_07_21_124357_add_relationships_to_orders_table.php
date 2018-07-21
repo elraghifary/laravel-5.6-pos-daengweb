1ce2f0a66ee1c0d49d8fb2376b2e72b66c0d592c
Elra Ghifary <elraghifary@gmail.com>
2018-07-21 19:44:53 +0700

    add relationship to order

---
+++ database/migrations/2018_07_21_124357_add_relationships_to_orders_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddRelationshipsToOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->integer('customer_id')->unsigned()->change();
+            $table->foreign('customer_id')->references('id')->on('customers')->onUpdate('cascade')->onDelete('cascade');
+                
+            $table->integer('user_id')->unsigned()->change();
+            $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function(Blueprint $table) {
+            $table->dropForeign('orders_customer_id_foreign');
+            $table->dropIndex('orders_customer_id_foreign');
+            $table->integer('customer_id')->change();
+            $table->dropForeign('orders_user_id_foreign');
+            $table->dropIndex('orders_user_id_foreign');
+            $table->integer('user_id')->change();
+        });
+    }
+}
