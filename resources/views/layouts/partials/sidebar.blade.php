ee65bb94d14ff2b1901f09731c73b87db3b2c57b
Elra Ghifary <elraghifary@gmail.com>
2018-07-21 22:08:57 +0700

    add dashboard menu on sidebar

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -17,7 +17,7 @@
     <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
             <li class="nav-item">
-                <a href="#" class="nav-link">
+                <a href="{{ route('home') }}" class="nav-link">
                     <i class="nav-icon fa fa-th"></i>
                     <p>Dashboard</p>
                 </a>
